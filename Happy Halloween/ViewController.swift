667a750215658e84cdd0cd6c3f62e0a819f1de4c
Cesar Medina <Cesar@Cesars-MacBook.local>
2015-11-05 15:38:11 -0800

    Added files for Happy Halloween.

--- Happy Halloween/ViewController.swift
+++ Happy Halloween/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HalloweenLogo: UIImageView!
+    @IBOutlet weak var HalloweenBG: UIImageView!
+    @IBOutlet weak var HalloweenButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HalloweenButtonAction(sender: AnyObject) {
+        HalloweenLogo.hidden = false
+        HalloweenBG.hidden = false
+        HalloweenButton.hidden = true
+    }
 
 }
 
